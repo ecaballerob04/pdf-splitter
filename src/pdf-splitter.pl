0d8b06a893241f93c019b8f70b0d2e6fa9202177
Emilio Caballero <milocaballerob04@gmail.com>
2021-09-25 15:34:58 -0500

    Use Text:CSV to read csv files and use arguments

--- src/pdf-splitter.pl
+++ src/pdf-splitter.pl
@@ -2,37 +2,49 @@ use strict;
 use warnings 'all';
 use utf8;
 use PDF::API2;
+use Text::CSV;
 
-my $pdfFileName = '../test/test.pdf';
-my $csvFileName = "../test/names.csv";
+my ($pdfFileName, $csvFileName, $destDir) = @ARGV;
 
-split_file($pdfFileName, $csvFileName );
+if( not defined $pdfFileName) {
+    die "PDF file need it";
+}
+if( not defined $csvFileName) {
+    die "CSV file need it";
+}
+if( not defined $destDir) {
+    die "Destination Folder need it";
+}
+
+
+
+my @rows = get_rows($csvFileName);
+split_file($pdfFileName, @rows);
 
 sub split_file {
-    my ($pdfFile, $csvFile) = @_;
+    my ($pdfFile, @rows) = @_;
     my $oldPdf = PDF::API2->open($pdfFile);
-    my @rows = get_rows($csvFile);
     for my $numPage ( 1 .. $oldPdf->pages() ) {
         my $newPdf = PDF::API2->new();
         my $page = $newPdf->import_page($oldPdf,$numPage);
-        $newPdf->saveas("../test/test-${rows[$numPage - 1]}.pdf");
+        $newPdf->saveas("${destDir}/${rows[$numPage - 1]}.pdf");
         $newPdf->release();
     }
     $oldPdf->release();
 }
-
 sub get_rows {
     my ($csvFile) = @_;
-    open my $fh, "<:encoding(utf8)", $csvFile or die "names.csv: $!";
+    my $csv = Text::CSV->new();
+    open my $fh, "<:encoding(utf8)", $csvFile or die "${csvFile}: $!";
     my @rows;
     while (my $line = <$fh>){
         chomp $line;
-        my @fields = split ",", $line;
-        #print "${fields[0]}\n";
-        push @rows, $fields[0] 
+        
+        if($csv->parse($line)) {
+            my @fields = $csv->fields();
+            push @rows, $fields[0] 
+        }
     }
     close $fh;
     return @rows;
 }
-
-
