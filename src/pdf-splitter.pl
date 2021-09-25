7849745abcf3100111c82ac89a586b15e481624f
Emilio Caballero <milocaballerob04@gmail.com>
2021-09-25 14:31:12 -0500

    Use a csv file as name file to new pdf files

--- src/pdf-splitter.pl
+++ src/pdf-splitter.pl
@@ -3,27 +3,27 @@ use warnings 'all';
 use utf8;
 use PDF::API2;
 
-my $mainFile = '../test/test.pdf';
-my $csvFile = "../test/names.csv";
+my $pdfFileName = '../test/test.pdf';
+my $csvFileName = "../test/names.csv";
 
-split_file($mainFile);
+split_file($pdfFileName, $csvFileName );
 
 sub split_file {
-    my ($file) = @_;
-    my $oldPdf = PDF::API2->open($file);
+    my ($pdfFile, $csvFile) = @_;
+    my $oldPdf = PDF::API2->open($pdfFile);
     my @rows = get_rows($csvFile);
     for my $numPage ( 1 .. $oldPdf->pages() ) {
         my $newPdf = PDF::API2->new();
         my $page = $newPdf->import_page($oldPdf,$numPage);
-        $newPdf->saveas("../test/test-${numPage}.pdf");
+        $newPdf->saveas("../test/test-${rows[$numPage - 1]}.pdf");
         $newPdf->release();
     }
     $oldPdf->release();
 }
 
 sub get_rows {
-    my ($file) = @_;
-    open my $fh, "<:encoding(utf8)", $file or die "names.csv: $!";
+    my ($csvFile) = @_;
+    open my $fh, "<:encoding(utf8)", $csvFile or die "names.csv: $!";
     my @rows;
     while (my $line = <$fh>){
         chomp $line;
